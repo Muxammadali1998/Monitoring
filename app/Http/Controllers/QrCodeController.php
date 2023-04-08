3117e9b2b05de64400f4ad02a324887323e301e1
Muxammadali98 <turgunaliyev.98@gmail.com>
2023-04-08 12:45:18 +0500

    svg_to_png

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Helpers\Traits\ApiResponcer;
 
 class QrCodeController extends Controller
 {
-    use ApiRespocer;
     public function qrcode($id)
     {
-        $svg = \QrCode::size(300)->generate($id);
-        return $this->success($svg,'',200);
+        $svg = \QrCode::size(300)->format('png')->generate($id);
+        return $svg;
     }
 }
